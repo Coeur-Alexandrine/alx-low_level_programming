eda5ddf47d9306ca21dc961cad8a8f0c7f76f605
Alexandra <abla.badohoun@gmail.com>
2022-09-09 07:40:54 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 #include <unistd.h>
 /**
  * main - main block
+ *
  * Return: Always 1 (Success)
  */
 int main (void)
 {
-	int s = strlen("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
 	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
