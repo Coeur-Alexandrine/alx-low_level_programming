91f3d9a84e7985df06d8fdeee2870bf559f9d946
Alexandra <abla.badohoun@gmail.com>
2022-09-13 15:57:28 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
-#include "_putchar.h"
+#include "main.h"
 
 /**
- * main - Prints _putchar followed by a new line
- *
- * Return: Always 0 (Success)
- */
+ *  * main - Prints _putchar, followed by a new line
+ *   *
+ *    * Return: 0
+ *     */
+
 int main(void)
 {
-	char *sh = "_putchar";
-
-
-	while (*sh)
-	{
-
-
-		_putchar(*sh);
-		sh++;
-											}
-
-
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	
-	return (0)
-
+	return (0);
+}
