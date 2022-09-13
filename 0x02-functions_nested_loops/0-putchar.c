d4337a2a170ab298cea769b70e8a6265c3cd4a35
Alexandra <abla.badohoun@gmail.com>
2022-09-13 14:32:17 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "_putchar.h"
+
+/**
+ * main - Prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+
+	while (*sh)
+	{
+
+
+		_putchar(*sh);
+		sh++;
+											}
+
+
+	_putchar('\n');
+	
+	return (0)
+
