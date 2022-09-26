e1ff23de1c4bb67db14bc2e6565716fd589bf455
Alexandra <abla.badohoun@gmail.com>
2022-09-26 11:24:01 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,14 +5,16 @@
  * @dest: memory area to be copied to
  * @src: memory area to be copied from 
  * @n: number of bytes to be copied
+ *
  * Return: pointer to the copied memory block
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
-for (i < n; i++)
-{
-dest[i] = src[i];
-}
-return (dest);
+	unsigned int i = 0;
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
