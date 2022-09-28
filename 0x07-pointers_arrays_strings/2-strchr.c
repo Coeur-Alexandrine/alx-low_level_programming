83d032058a37a389f461e31ebbad2cef7856187e
Alexandra <abla.badohoun@gmail.com>
2022-09-28 07:17:45 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,6 @@
  * _strchr - locates a character in a string
  * @s: string to check
  * @c: character to check for
- *
  * Return: pointer to spot in s with c or null
  */
 char *_strchr(char *s, char c)
@@ -16,6 +15,7 @@ for (i = 0; *(s + i); i++)
 if (*(s + i) == c)
 return (s + i);
 }
-if (*(s +i) == c)
+if (*(s + i) == c)
+return (s + i);
 return (0);
 }
