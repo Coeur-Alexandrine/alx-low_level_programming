1aa064d81e17533698d21996069f0eb49fd8d43b
Alexandra <abla.badohoun@gmail.com>
2022-09-30 19:50:03 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,13 +7,13 @@
  * main - main function
  * @argc: argumentc
  * @argv: vector of arguments
- *
  * Return: always 0
  */
 
 int main(int argc, char  *argv[])
 {
 int coins = 0;
+
 if (argc == 2)
 {
 if (strchr(argv[argc - 1], '-'))
@@ -22,7 +22,9 @@ printf("0\n");
 return (1);
 }
 int money;
+
 money = atoi(argv[argc - 1]);
+
 while (money > 0)
 {
 if (money % 25 == 0)
@@ -43,7 +45,7 @@ money--;
 }
 coins++;
 }
-printf("%d\n", coins);
+printf("%i\n", coins);
 return (0);
 }
 printf("Error\n");
