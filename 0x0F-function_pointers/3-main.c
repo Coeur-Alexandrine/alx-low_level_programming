4bea5724adcc23bee54e5b7ee803d44650c5428f
Alexandra <abla.badohoun@gmail.com>
2022-10-12 15:10:29 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,12 +5,14 @@
  * main - program that perfroms simple operations
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
 int arg1, arg2, result;
+
 char o;
 int (*func)(int, int);
 
