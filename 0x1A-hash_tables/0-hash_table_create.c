2f9d3759eda300b4c32cf889a7389cadc18b7ed0
Alexandra <abla.badohoun@gmail.com>
2023-01-10 02:11:26 -0800

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 unsigned long int i;
 hash_table_t *hashtable;
+
 if (size == 0)
 return (NULL);
 hashtable = malloc(sizeof(hash_table_t));
